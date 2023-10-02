b8917d371d0d0420e8b0116b04e242337da497c6
Derwin <derwin9227@gmail.com>
2023-10-01 20:57:26 -0500

    agregando numero de ws

--- src/Components/whatsapp/Ws.jsx
+++ src/Components/whatsapp/Ws.jsx
@@ -4,7 +4,7 @@ const Ws = () => {
   return (
     <>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"></link>
-      <a href="https://api.whatsapp.com/send?phone=0000000&amp;text=Hola%21%20Quisiera%20solicitar%20tus%20servicios%20por%20favor%21"
+      <a href="https://api.whatsapp.com/send?phone=16503377613&amp;text=Hola%21%20Quisiera%20solicitar%20tus%20servicios%20por%20favor%21"
       className="float" 
       target="_blank"
       rel='noreferrer'
