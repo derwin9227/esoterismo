6d9d1921436c628ee5d359d5cd63b173314c00d1
Derwin <derwin9227@gmail.com>
2023-10-05 19:58:06 -0500

    manejando formulario y enviados datos a correo

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,24 +1,52 @@
+import { useState } from "react";
 import "./contact.scss";
 
 const About = () => {
+  const [form, setForm] = useState({});
+  //const validarEmail = email => ((/[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})/i.test(email)));
+
+  const handleChange = e => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+
+    //alert("inicio");
+    //alert(e.correo);
+    console.log(form);
+    //validarEmail(e.target.correo) ? alert("correo correcto") : alert("correo incorrecto");
+    //e.submit();
+    //alert("fin");
+  };
+
+  /*  Regex email
+  ^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$
+   */
+
   return (
     <section id="contact" className="contact">
       <div className="contact__heading">
         <h2>Contactanos</h2>
       </div>
       
-      <form action="" className="contact__form">
+      <form  className="contact__form" 
+      action="https://formspree.io/f/mgejpdkp" method="POST">
         <div className="contact__form-input">
-              <input className="contact__form-input-name" name="nombre" type="text" placeholder="Nombre"/>
-              <input className="contact__form-input-email" name="correo" type="text" placeholder="Correo"/>
-              <input className="contact__form-input-telf" name="telefono" type="text" placeholder="Telefono"/>
+              <input className="contact__form-input-name" name="nombre" type="text" placeholder="Nombre" onChange={handleChange}/>
+              <input className="contact__form-input-email" name="correo" type="email" placeholder="Correo" onChange={handleChange}/>
+              <input className="contact__form-input-telf" name="telefono" type="number" placeholder="Telefono" onChange={handleChange}/>
         </div>
-        <textarea name="asunto"
-               cols="30" rows="5"
-               className="contact__form-subject"
-               placeholder="Cuentanos que sucede...">
+        <textarea name="asunto" /* type="text" */
+              onChange={handleChange}
+              className="contact__form-subject"
+              placeholder="Cuentanos que sucede..."
+              >
         </textarea>
-        <button className="contact__form-button">Enviar</button>
+        <button type="submit" className="contact__form-button"
+          /* onClick={handleSubmit} */
+        >
+          Enviar
+        </button>
       </form>
     </section>
   )
